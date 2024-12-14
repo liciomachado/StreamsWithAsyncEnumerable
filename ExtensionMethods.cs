21898a088f571036c7942f4e35049b4dfdbf0776
Maurício Machado da Rosa <mauricio.rosa@agrotools.com.br>
2024-12-14 18:33:22 -0300

    Add project files.

---
+++ ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿namespace StreamsWithAsyncEnumerable;
+
+public static class ExtensionMethods
+{
+    public static IServiceCollection CustomAddCors(this IServiceCollection service)
+    {
+        service.AddCors(options =>
+        {
+            options.AddPolicy("AllowAllOrigins",
+                builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+        });
+        return service;
+    }
+}
